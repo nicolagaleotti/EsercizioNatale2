3b7c6cb9144133224b0575a3fdd5b3d891097fb6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-20 09:32:04 +0100

    fine

--- Natale/MainWindow.xaml.cs
+++ Natale/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Natale
 {
@@ -23,6 +24,66 @@ namespace Natale
         public MainWindow()
         {
             InitializeComponent();
+            cmbOperazioni.Items.Add("Addizione");
+            cmbOperazioni.Items.Add("Sottrazione");
+            cmbOperazioni.Items.Add("Moltiplicazione");
+            cmbOperazioni.Items.Add("Divisione");
+
+        }
+
+        string file = "operazioni.txt";
+
+        private void btnCalcola_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double n1 = double.Parse(txt1.Text);
+                if (n1 == double.NaN)
+                {
+                    throw new Exception("Inserisci il primo numero!");
+                }
+                double n2 = double.Parse(txt2.Text);
+                if (n1 == double.NaN)
+                {
+                    throw new Exception("Inserisci il secondo numero!");
+                }
+                string risultato = "";
+            
+                if (cmbOperazioni.SelectedItem == null)
+                {
+                    throw new Exception("Devi selezionare un'operazione!");
+                }
+                if (cmbOperazioni.SelectedIndex == 0)
+                {
+                    risultato = (n1 + n2).ToString();
+                }
+                if (cmbOperazioni.SelectedIndex == 1)
+                {
+                    risultato = (n1 - n2).ToString();
+                }
+                if (cmbOperazioni.SelectedIndex == 2)
+                {
+                    risultato = (n1 * n2).ToString();
+                }
+                if (cmbOperazioni.SelectedIndex == 3)
+                {
+                    risultato = (n1 / n2).ToString();
+                    if (n2 == 0)
+                        risultato = "Divisione impossibile";
+                    if (n1 == 0 && n2 == 0)
+                        risultato = "Divisione indeterminata";
+                }
+                txtRisultato.Text = $"Il risultato è : {risultato}";
+                using (StreamWriter writer = new StreamWriter(file, true))
+                {
+                    writer.WriteLine(txtRisultato.Text);
+                    writer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
